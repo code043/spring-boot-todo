a095038914084668c6b803de2085bd16e4fdacd8
code043 <procoder043@gmail.com>
2024-11-29 16:11:26 -0300

    TodoService

---
+++ src/main/java/com/code043/todolist/service/TodoService.java
@@ -0,0 +1,29 @@
+package com.code043.todolist.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.code043.todolist.entity.Todo;
+import com.code043.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+  private TodoRepository todoRepository;
+
+ public List<Todo> create(Todo todo){
+  todoRepository.save(todo);
+  return list();
+ }
+ public List<Todo> list(){
+  return todoRepository.findAll();
+ }
+ public List<Todo> update(Todo todo){
+  todoRepository.save(todo);
+  return list();
+ }
+ public List<Todo> delete(Long id){
+  todoRepository.deleteById(id);
+  return list();
+ }
+}
